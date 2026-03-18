31d213f4953a47fccd027d76715b169e1abe42b0
Krishita22 <krishitavaghani@gmail.com>
2026-03-18 19:19:16 -0400

    feat: listings, claim-match workflows, filtering, and editing improvements

--- unifind_flutter/test/widget_test.dart
+++ unifind_flutter/test/widget_test.dart
@@ -48,6 +48,7 @@ void main() {
       condition: 'Good',
       image: 'https://example.com/image.png',
       seller: 'tester',
+      sellerEmail: 'tester@montclair.edu',
       createdAt: DateTime(2026, 2, 26),
       location: 'Blanton Hall',
     );
@@ -98,6 +99,8 @@ void main() {
             onListItem: () {
               tapped = true;
             },
+            onEditMarketplace: (_, __) async {},
+            onEditLostFound: (_, __) async {},
           ),
         ),
       ),
