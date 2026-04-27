0405ab172894a7d090e0a92e39546ab80dc00d99
Krishita22 <krishitavaghani@gmail.com>
2026-04-27 13:50:32 -0400

    Fix flutter_bootstrap.js syntax error in serviceWorkerVersion

--- unifind_flutter/build/web/flutter_bootstrap.js
+++ unifind_flutter/build/web/flutter_bootstrap.js
@@ -41,6 +41,6 @@ _flutter.loader.load({
     canvasKitBaseUrl: "https://www.gstatic.com/flutter-canvaskit/6c0baaebf70e0148f485f27d5616b3d3382da7bf/",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: ""3474951483" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */"
+    serviceWorkerVersion: "3474951483" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
